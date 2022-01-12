a49e89c3839fa3abb6abeb3afa278d39687f0ce0
Nikita Bloshchanevich <nikblos@outlook.com>
2022-01-12 21:16:15 +0100

    Add coverage.py use module and branch coverage settings to Coverage window

--- src/main/java/org/nbfalcon/pycharmCoverage/coveragePy/CoveragePyViewExtension.java
+++ src/main/java/org/nbfalcon/pycharmCoverage/coveragePy/CoveragePyViewExtension.java
@@ -7,17 +7,47 @@ import com.intellij.coverage.view.DirectoryCoverageViewExtension;
 import com.intellij.coverage.view.ElementColumnInfo;
 import com.intellij.coverage.view.PercentageCoverageColumnInfo;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.ui.ColumnInfo;
 import com.jetbrains.python.PythonFileType;
+import org.jetbrains.annotations.NotNull;
 import org.nbfalcon.pycharmCoverage.i18n.PycharmCoverageBundle;
+import org.nbfalcon.pycharmCoverage.settings.PycharmCoverageProjectSettings;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CoveragePyViewExtension extends DirectoryCoverageViewExtension {
+    @Override
+    public @NotNull List<AnAction> createExtraToolbarActions() {
+        return List.of(new ToggleAction("Branch coverage") {
+            @Override
+            public boolean isSelected(@NotNull AnActionEvent anActionEvent) {
+                return PycharmCoverageProjectSettings.getInstance(myProject).enableBranchCoverage;
+            }
+
+            @Override
+            public void setSelected(@NotNull AnActionEvent anActionEvent, boolean b) {
+                PycharmCoverageProjectSettings.getInstance(myProject).enableBranchCoverage = b;
+            }
+        }, new ToggleAction("coverage.py: Use Module") {
+            @Override
+            public boolean isSelected(@NotNull AnActionEvent anActionEvent) {
+                return PycharmCoverageProjectSettings.getInstance(myProject).coveragePyUseModule;
+            }
+
+            @Override
+            public void setSelected(@NotNull AnActionEvent anActionEvent, boolean b) {
+                PycharmCoverageProjectSettings.getInstance(myProject).coveragePyUseModule = b;
+            }
+        });
+    }
+
     public CoveragePyViewExtension(Project project, CoverageAnnotator annotator, CoverageSuitesBundle suitesBundle, CoverageViewManager.StateBean stateBean) {
         super(project, annotator, suitesBundle, stateBean);
     }
